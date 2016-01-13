4e321a171e406f7357bf827c1430a7b384f8532e
vicksonzero <dicksonzero@gmail.com>
2016-01-13 18:57:51 +0800

    more code for loading files

--- js/bundle.js
+++ js/bundle.js
@@ -2,31 +2,55 @@
 // include
 var $ = require('jquery');
 
+// global variable
+var template_svgData = null;
+var template_dataURL = null;
+var dataSheet = null;
 
 // dom operations
 
 $("#input-template-file").change(function(){
 	console.log(this.files);
 
+	if(this.files.length<=0){
+		return;
+	}
 	// grab the first image in the FileList object and pass it to the function
 	renderImage(this.files[0]);
 });
+
+$("#input-datasheet-file").change(function(){
+	console.log(this.files);
+
+	if(this.files.length<=0){
+		return;
+	}
+	// grab the first image in the FileList object and pass it to the function
+	renderImage(this.files[0]);
+});
+
+
+
+
+
 
 // render the image in our view
 function renderImage(file) {
 
 	// generate a new FileReader object
 	var reader = new FileReader();
-
 	// inject an image with the src url
 	reader.onload = function(event) {
-		var the_url = event.target.result
-		$("#preview-template-file").attr("src",the_url);
+		var template_dataURL = event.target.result;
+		$("#preview-template-file").attr("src", template_dataURL);
 	}
-
 	// when the file is read it triggers the onload event above.
 	reader.readAsDataURL(file);
 }
+
+var p1 = new Promise(function(resolve, reject) {
+	console.log("HI");
+});
 
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
